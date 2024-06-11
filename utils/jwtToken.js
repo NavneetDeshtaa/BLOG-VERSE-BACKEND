eadde03eabaa4525803728f9a6a92c5db9437e2b
Navneet Deshta <navneetdeshtaa@gmail.com>
2024-06-11 12:18:39 +0530

    Initial commit

---
+++ utils/jwtToken.js
@@ -0,0 +1,17 @@
+export const sendToken =  async (user, statusCode, message, res) => {
+    const token = await user.getJWTToken();
+    const options = {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    };
+    res.status(statusCode).cookie("token", token, options).json({
+      success: true,
+      user,
+      message,
+      token,
+    });
+  };
